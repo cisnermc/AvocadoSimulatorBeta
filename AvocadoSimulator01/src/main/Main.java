d19c20aad708e45bd605df58f52e00c3b103871a
cisnermc <cisnermc@LCBLAB209-12.uccob.uc.edu>
2019-04-23 09:12:08 -0400

    Fixed Visibility of enum in Avocado Class

--- AvocadoSimulator01/src/main/Main.java
+++ AvocadoSimulator01/src/main/Main.java
@@ -3,17 +3,25 @@
  * nicholdw@ucmail.uc.edu
  */
 package main;
+
+import avocado.Avocado;
+import avocado.Avocado.enumCondition;
+
 /***
  * Exercise our Avocado classes
  * @author nicomp
  *
  */
 public class Main {
-	enum myEnum {A,B,C,D};	// Demo of enumerated date type
+	//enum myEnum {A,B,C,D};	// Demo of enumerated date type
 
 	public static void main(String[] args) {
-		myEnum e = myEnum.A;
-		System.out.println(e.toString());
+		// Declare and instantiate an avocado
+		Avocado bob = new Avocado("Brownish-Green", Avocado.enumCondition.fresh, 10);
+		
+		
+		//myEnum e = myEnum.A;
+		//System.out.println(e.toString());
 
 	}
 }
